5463b62d7413428a070ef21e734c08b65822d0aa
Abhishek <abhishekk19@gmail.com>
2018-08-10 19:35:57 +0530

    shell script modified, added prompts with color on stdout

--- setpath.sh
+++ setpath.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+GREEN="\033[92m"
+DEFAULT="\033[0m"
+
+success_print() {
+  echo -en "[+]$GREEN $1 $DEFAULT"
+  echo
+}
+
 cd $(dirname "$0")
 CWD=$(pwd -L)
 cd - > /dev/null
 FILES=$(ls | grep -v $(basename $0))
 
 for dir in $FILES; do
+  success_print "working on $dir"
   ln -rsf $CWD/$dir ~/.$dir
 done
+
 
