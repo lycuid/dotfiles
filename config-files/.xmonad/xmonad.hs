cd44f4e1550dc567219da1010e523b46045e2026
Abhishek <abhishekk19@gmail.com>
2020-06-04 14:20:58 +0530

    renamed all files, prepended with (.)dot.

--- config-files/.xmonad/xmonad.hs
+++ config-files/.xmonad/xmonad.hs
@@ -1,9 +1,15 @@
 import XMonad
-import XMonad.Util.Run
-import XMonad.Util.SpawnOnce
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.DynamicLog
-import XMonad.Layout.Spacing
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
+import XMonad.Util.SpawnOnce (spawnOnce)
+import XMonad.Hooks.ManageDocks (docks, avoidStruts)
+import XMonad.Layout.Spacing (spacingRaw, Border(..))
+
+import XMonad.Hooks.DynamicLog  ( dynamicLogWithPP
+                                , wrap
+                                , xmobarPP
+                                , xmobarColor
+                                , PP(..)
+                                )
 
 import Data.Monoid
 import Text.Printf (printf)
@@ -164,7 +170,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 --
 myLayout = avoidStruts $ borders $ tiled ||| Mirror tiled ||| Full
   where
-    borders = spacingRaw True (Border 0 0 0 0) False (Border 3 3 3 3) True
+    borders = spacingRaw False (Border 0 0 0 0) False (Border 3 3 3 3) True
 
     -- default tiling algorithm partitions the screen into two panes
     tiled   = Tall nmaster delta ratio
@@ -265,61 +271,3 @@ main = do
     , startupHook        = myStartupHook
     }
 
-
-{--
- -- launching and killing programs
- -- ----------------------------------------------------------------------
- -- mod-Shift-Enter  Launch xterminal
- -- mod-p            Launch dmenu
- -- mod-Shift-p      Launch gmrun
- -- mod-Shift-c      Close/kill the focused window
- -- mod-Space        Rotate through the available layout algorithms
- -- mod-Shift-Space  Reset the layouts on the current workSpace to default
- -- mod-n            Resize/refresh viewed windows to the correct size
-
- -- move focus up or down the window stack
- -- ----------------------------------------------------------------------
- -- mod-Tab        Move focus to the next window
- -- mod-Shift-Tab  Move focus to the previous window
- -- mod-j          Move focus to the next window
- -- mod-k          Move focus to the previous window
- -- mod-m          Move focus to the master window
-
- -- modifying the window order
- -- ----------------------------------------------------------------------
- -- mod-Return   Swap the focused window and the master window
- -- mod-Shift-j  Swap the focused window with the next window
- -- mod-Shift-k  Swap the focused window with the previous window
-
- -- resizing the master/slave ratio
- -- ----------------------------------------------------------------------
- -- mod-h  Shrink the master area
- -- mod-l  Expand the master area
-
- -- floating layer support
- -- ----------------------------------------------------------------------
- -- mod-t  Push window back into tiling; unfloat and re-tile it
-
- -- increase or decrease number of windows in the master area
- -- ----------------------------------------------------------------------
- -- mod-comma  (mod-,)   Increment the number of windows in the master area
- -- mod-period (mod-.)   Deincrement the number of windows in the master area
-
- -- quit, or restart
- -- ----------------------------------------------------------------------
- -- mod-Shift-q  Quit xmonad
- -- mod-q        Restart xmonad
- -- mod-[1..9]   Switch to workSpace N
-
- -- Workspaces & screens
- -- ----------------------------------------------------------------------
- -- mod-Shift-[1..9]   Move client to workspace N
- -- mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3
- -- mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3
-
- -- Mouse bindings: default actions bound to mouse events
- -- ----------------------------------------------------------------------
- -- mod-button1  Set the window to floating mode and move by dragging
- -- mod-button2  Raise the window to the top of the stack
- -- mod-button3  Set the window to floating mode and resize by dragging
- --}
