83e20108ac349253b1369a0a7c9fa446c76512d4
Abhishek <abhishekk19@gmail.com>
2020-06-04 14:31:02 +0530

    refactor.

--- setup.sh
+++ setup.sh
@@ -2,14 +2,19 @@
 
 DEFAULT="\033[0m"
 GREEN="\033[92m"
+YELLOW="\033[93m"
 BLUE="\033[94m"
 
 echo_success() {
   echo -e "$GREEN$1$DEFAULT"
 }
 
-echo_warning() {
+echo_info() {
   echo -e "$BLUE$1$DEFAULT"
+}
+
+echo_warning() {
+  echo -e "$YELLOW$1$DEFAULT"
 }
 
 cwd="./config-files"
@@ -26,7 +31,7 @@ for path in $paths; do
     if [ -d $CONFIG/$path ]; then
       echo_warning "[?] Ignoring directory: $path"
     else
-      echo_success "[+] Creating empty directory: $path"
+      echo_info "[+] Creating directory: $path"
       mkdir $CONFIG/$path
     fi
   fi
