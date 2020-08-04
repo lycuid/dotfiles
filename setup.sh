4840220916d430bb1ffd21dead7757fba213b63c
Abhishek Kadam <abhishekk19@gmail.com>
2020-08-04 23:27:45 +0530

    setup script updated (linking and delinking).

--- setup.sh
+++ setup.sh
@@ -1,40 +1,81 @@
-#!/bin/bash
+#!/bin/sh
+
+
+if [ $# -lt 2 ]; then
+  echo "Not enough arguments."
+  exit 1
+fi
+
+getRelativePath() {
+  echo $(cd $(dirname $1); pwd)/$(basename $1)
+}
+
+query=$1
+src=$(getRelativePath $2)
+[ $3 ] && dest=$(getRelativePath $3) || dest=$HOME
 
 DEFAULT="\033[0m"
 GREEN="\033[92m"
 YELLOW="\033[93m"
 BLUE="\033[94m"
 
-echo_success() {
-  echo -e "$GREEN$1$DEFAULT"
-}
 
-echo_info() {
-  echo -e "$BLUE$1$DEFAULT"
-}
+link() {
+  paths=$(
+    cd $(dirname $src) && tree -iaf --noreport $(basename $src) \
+      | sed "1d" \
+      | cut -d \/ -f 2-
+  )
 
-echo_warning() {
-  echo -e "$YELLOW$1$DEFAULT"
-}
+  for path in $paths; do
+    s=$src/$path
+    d=$dest/$path
 
-ROOT="$HOME"
-CWD="./files"
-PATHS=$(tree $CWD -iaf --noreport | cut -s -d \/ -f 3-)
-
-for path in $PATHS; do
-  # if file, create a symlink.
-  if [ -f "$CWD/$path" ]; then
-    echo_success "[+] Linking: $path"
-    ln -rsf $CWD/$path $ROOT/$path
-  else
-    # else if directory, mkdir if doesn't exist already.
-    if [ -d $ROOT/$path ]; then
-      echo_warning "[?] Ignoring directory: $path"
+    if [ -f $s ]; then
+      echo "$GREEN[+] Linking: $path$DEFAULT"
+      ln -rsf $s $d
     else
-      echo_info "[+] Creating directory: $path"
-      mkdir $ROOT/$path
+      if [ -d $d ]; then
+        echo "$YELLOW[?] Ignoring directory: $path$DEFAULT"
+      else
+        echo "$BLUE[+] Creating directory: $path$DEFAULT"
+        mkdir $d
+      fi
     fi
-  fi
-done
+  done
+}
 
+delink() {
+  paths=$(
+    cd $(dirname $src) && tree --dirsfirst -iaf --noreport $(basename $src) \
+      | sed "1d" \
+      | cut -d \/ -f 2- \
+      | sed '1!G;h;$!d'
+  )
 
+  for path in $paths; do
+    d=$dest/$path
+    if [ -f $d ]; then
+      echo "$GREEN[+] Delinking: $path$DEFAULT"
+      rm $d
+    fi
+
+    if [ -d $d ] && [ $(ls $d | wc -l) -eq 0 ]; then
+      echo "$BLUE[+] Removing Empty Directory: $path$DEFAULT"
+      rm -rf $d
+    fi
+  done
+}
+
+case $query in
+  "link")
+    link
+    ;;
+  "delink")
+    delink
+    ;;
+  *)
+    echo "Invalid Query."
+    exit 1
+    ;;
+esac
