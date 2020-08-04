f5c7060649ecec76a8864f684334586ee4b3b22d
Abhishek Kadam <abhishekk19@gmail.com>
2020-08-04 23:48:45 +0530

    vscode updae script updated (now removes tmp downloaded file).

--- update-vscode-deb.sh
+++ update-vscode-deb.sh
@@ -1,3 +1,9 @@
 # !/bin/bash/
-wget -rvO /tmp/code_latest_amd64.deb https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
+wget -rvO /tmp/code_latest_amd64.deb \
+  https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
 sudo dpkg -i /tmp/code_latest_amd64.deb
+
+echo "[+] Installation Completed."
+echo "[+] Cleaning up."
+rm -rf /tmp/code_latest_amd64.deb
+echo "[+] Done."
