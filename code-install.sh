079cd4705d35f172ba298660c31153017e534dc2
Abhishek Kadam <abhishekk19@gmail.com>
2020-08-19 23:50:31 +0530

    vscode scripts refactoring and relocations.

--- code-install.sh
+++ code-install.sh
@@ -2,8 +2,4 @@
 wget -rvO /tmp/code_latest_amd64.deb \
   https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
 sudo dpkg -i /tmp/code_latest_amd64.deb
-
-echo "[+] Installation Completed."
-echo "[+] Cleaning up."
 rm -rf /tmp/code_latest_amd64.deb
-echo "[+] Done."
