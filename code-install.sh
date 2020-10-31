7f84ecb56ca715b57360d275936d4095b0cd9bb6
Abhishek Kadam <abhishekk19@gmail.com>
2020-10-31 23:32:23 +0530

    vscode settings, keybindings, extensions etc now synced with vscode's 'Settings sync' feature, no longer required to save settings etc with version control.

--- code-install.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash/
-wget -rvO /tmp/code_latest_amd64.deb \
-  https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
-sudo dpkg -i /tmp/code_latest_amd64.deb
-rm -rf /tmp/code_latest_amd64.deb
