4d44562bdcfb4e6e5c81aad2be19c642a7e21a07
Abhishek Kadam <abhishekk19@gmail.com>
2020-11-22 12:45:59 +0530

    Xresources: dmenu colors updated. dunst: styling updated, icon added. gitconfig: gpg sign. XPrompt: refactoring. dmenu_launcher: no center, border. icons and wallpapers added.

--- .config/xmonad/lib/Configs/XPrompt.hs
+++ .config/xmonad/lib/Configs/XPrompt.hs
@@ -16,29 +16,29 @@ import Configs.Colors           (Colors(..))
 
 xpConfig :: XPConfig
 xpConfig =
-    def
-      { font                  = "xft:IBM Plex Mono-11"
-      , fgColor               = white def
-      , bgColor               = black def
-      , bgHLight              = cyan def
-      , fgHLight              = black def
-      , borderColor           = blue def
-      , promptBorderWidth     = 2
-      , position              = CenteredAt 0.2 0.5
-      , alwaysHighlight       = False
-      , height                = 30
-      , promptKeymap          = emacsLikeXPKeymap
-      , historySize           = 64
-      , searchPredicate       = isInfixOf
-      , maxComplRows          = Just 5
-      -- , historyFilter :: [String] -> [String]
-      -- , completionKey :: (KeyMask, KeySym)
-      -- , changeModeKey :: KeySym
-      -- , defaultText :: String
-      -- , autoComplete :: Maybe Int
-      -- , showCompletionOnTab :: Bool
-      -- , searchPredicate :: String -> String -> Bool
-      }
+  def
+    { font                  = "xft:IBM Plex Mono-11"
+    , fgColor               = white def
+    , bgColor               = black def
+    , bgHLight              = cyan def
+    , fgHLight              = black def
+    , borderColor           = blue def
+    , promptBorderWidth     = 2
+    , position              = CenteredAt 0.2 0.5
+    , alwaysHighlight       = False
+    , height                = 30
+    , promptKeymap          = emacsLikeXPKeymap
+    , historySize           = 64
+    , searchPredicate       = isInfixOf
+    , maxComplRows          = Just 5
+    -- , historyFilter :: [String] -> [String]
+    -- , completionKey :: (KeyMask, KeySym)
+    -- , changeModeKey :: KeySym
+    -- , defaultText :: String
+    -- , autoComplete :: Maybe Int
+    -- , showCompletionOnTab :: Bool
+    -- , searchPredicate :: String -> String -> Bool
+    }
 
 shellXPrompt :: X ()
 shellXPrompt = shellPrompt xpConfig
@@ -53,7 +53,7 @@ editorXPConfig = xpConfig
 {- @TODO: write a custom 'getShellCompl' function. -}
 editorXPrompt :: XConfig Layout -> X ()
 editorXPrompt conf = mkXPrompt Editor editorXPConfig (getShellCompl [cmd] $ searchPredicate editorXPConfig) run
-    where
-      run a  = unsafeSpawn $ cmd ++ " " ++ a
-      cmd    = unwords [XMonad.terminal conf, "-e", myEditor]
+  where
+    run a  = unsafeSpawn $ cmd ++ " " ++ a
+    cmd    = unwords [XMonad.terminal conf, "-e", myEditor]
 
